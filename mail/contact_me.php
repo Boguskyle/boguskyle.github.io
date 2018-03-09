4d352ddb13d1a2b7a90899bd4aae205abb3fe313
Kyle <boguskyle@gmail.com>
2018-03-09 09:09:14 -0800

    PHP try 3

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,6 @@
 <?php
+
+
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -19,7 +21,7 @@ $business = strip_tags(htmlspecialchars($_POST['business']));
 $to = 'boguskyle@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "New contact request for ReviewMate.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nBusiness:\n$business";
-$headers = "From: noreply@pickerson.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@pickerson.com \r\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
